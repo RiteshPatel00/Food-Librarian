83c6d56b9cf0c2a4a4b00666cef7fe1233362e8d
Ritesh Patel <patel2000ritesh@gmail.com>
2021-12-08 22:39:19 -0500

    Add comments

--- connect.php
+++ connect.php
@@ -1,6 +1,7 @@
 <!-- Connect PHP file to connect to our server's database -->
 <?php
 try {
+    // For SQL injection prevention, all of our database queries were done using PDO parameterized queries
     $db = new PDO('mysql:host=localhost;dbname=library', 'librarian', 'password123');
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
